757b16bf4cf0bbc68151b42d10b67c21116eb930
Ederson Grein <ederson@esss.com.br>
2016-11-30 11:02:01 -0200

    created dm shell methods for creating vectors and matrices

--- ref_case.c
+++ ref_case.c
@@ -21,6 +21,76 @@ void configure_petsc_options() {
 }
 
 #undef __FUNCT__
+#define __FUNCT__ "CreateGlobalVector"
+static PetscErrorCode CreateGlobalVector(DM shell, Vec *x)
+{
+	PetscErrorCode	ierr;
+	DM				*local_dms, composite;
+	PetscInt		nDMs, local_size, dm_size, rank;
+
+	MPI_Comm_rank(PETSC_COMM_WORLD, &rank);
+	ierr = DMShellGetContext(shell, (void**)&composite); CHKERRQ(ierr);
+	ierr = DMCompositeGetNumberDM(composite, &nDMs);
+	PetscMalloc1(nDMs, &local_dms);
+	DMCompositeGetEntriesArray(composite, local_dms);
+	local_size = 0;
+	for (int p = 0; p < nDMs; p++) {
+		DMType dm_type;
+		DMGetType(local_dms[p], &dm_type);
+		
+		PetscBool type_matched = PETSC_FALSE;
+		PetscStrcmp(dm_type, DMDA, &type_matched);
+		if (type_matched) {
+			DMDAGetCorners(local_dms[p], NULL, NULL, NULL, &dm_size, NULL, NULL);
+		}
+
+		PetscStrcmp(dm_type, DMREDUNDANT, &type_matched);
+		if (type_matched) {
+			PetscInt prc_owner;
+			DMRedundantGetSize(local_dms[p], &prc_owner, &dm_size);
+			if (prc_owner != rank) {
+				dm_size = 0;
+			}
+		}
+
+		local_size += dm_size;
+	}
+
+	VecCreate(PETSC_COMM_WORLD, x);
+	VecSetSizes(*x, local_size, PETSC_DECIDE);
+
+	ierr = VecSetDM(*x, shell); CHKERRQ(ierr);
+	
+	ierr = VecSetFromOptions(*x); CHKERRQ(ierr);
+	PetscFree(local_dms);
+
+	return 0;
+}
+
+#undef __FUNCT__
+#define __FUNCT__ "CreateMatrix"
+static PetscErrorCode CreateMatrix(DM shell, Mat *A)
+{
+	PetscErrorCode	ierr;
+	DM				composite;
+	Mat				local_A;
+	PetscInt		prc_size;
+
+	ierr = DMShellGetContext(shell, (void**)&composite); CHKERRQ(ierr);
+	MPI_Comm_size(PETSC_COMM_WORLD, &prc_size);
+
+	if (prc_size == 1) {
+		ierr = DMCreateMatrix(composite, A); CHKERRQ(ierr);
+		return 0;
+	}
+
+	ierr = DMCreateMatrix(composite, &local_A); CHKERRQ(ierr);
+	ierr = MatCreateMPIMatConcatenateSeqMat(PETSC_COMM_WORLD, local_A, PETSC_DECIDE, MAT_INITIAL_MATRIX, A); CHKERRQ(ierr);
+
+	return 0;
+}
+
+#undef __FUNCT__
 #define __FUNCT__ "main"
 int main(int argc, char **argv) {
 
@@ -31,7 +101,7 @@ int main(int argc, char **argv) {
 	double dt = 0.001;
 	double dt_min = 1.0e-3;
 	double dt_max = 10.0;
-	double final_time = 0.01;
+	double final_time = 100.0;
 
 	PetscInt nx = 2;
 	PetscOptionsGetInt(NULL, NULL, "-nx", &nx, NULL);
@@ -39,6 +109,37 @@ int main(int argc, char **argv) {
 	int rank, n_prcs;
 	MPI_Comm_rank(PETSC_COMM_WORLD, &rank);
 	MPI_Comm_size(PETSC_COMM_WORLD, &n_prcs);
+
+	double initial_temperature = 10.0;
+	double conductivity = 1.0;
+	double source_term = 0.0;
+	double wall_length = 1.0;
+
+	PetscInt dof = 1;
+	PetscInt stencil_width = 1;
+
+	Params params;
+	params.conductivity_ = 1.0;
+	params.source_term_ = 0.0;
+	params.wall_length_ = 1.0;
+	PetscMalloc1(4, &params.temperature_presc_);
+	params.temperature_presc_[0] = 2.0;
+	params.temperature_presc_[1] = 10.0;
+	params.temperature_presc_[2] = 55.0;
+	params.temperature_presc_[3] = 60.0;
+
+	TS ts;
+	TSCreate(PETSC_COMM_WORLD, &ts);
+
+	////////////////////////////////////////////////////////////
+	////				Single pipe							////
+	////////////////////////////////////////////////////////////
+	//DM domain;
+	//DMDACreate1d(PETSC_COMM_WORLD, DM_BOUNDARY_GHOSTED, nx, dof, stencil_width, NULL, &domain);
+
+	////////////////////////////////////////////////////////////
+	////				SHELL DM 							////
+	////////////////////////////////////////////////////////////
 
 	PetscInt prc_owner[PIPES_SIZE];
 	int owner = 0;
@@ -49,62 +150,64 @@ int main(int argc, char **argv) {
 		prc_owner[i] = owner++;
 	}
 
-	double temperature_presc[] = { 2.0, 50.0, 55.0, 60.0 };
+	DM subdomain;
+	DMCompositeCreate(PETSC_COMM_SELF, &subdomain);
 
-	double initial_temperature = 10.0;
-	double conductivity = 1.0;
-	double source_term = 0.0;
-	double wall_length = 1.0;
-
-	Params params;
-	params.conductivity_ = 1.0;
-	params.source_term_ = 0.0;
-	params.wall_length_ = 1.0;
-	PetscMalloc1(4, &params.temperature_presc_);
-	params.temperature_presc_[0] = 2.0;
-	params.temperature_presc_[1] = 50.0;
-	params.temperature_presc_[2] = 55.0;
-	params.temperature_presc_[3] = 60.0;
-
-	TS ts;
-	TSCreate(PETSC_COMM_WORLD, &ts);
-
-	//// Single pipe case
-	//DM domain;
-	//PetscInt dof = 1;
-	//PetscInt stencil_width = 1;
-	//DMDACreate1d(PETSC_COMM_WORLD, DM_BOUNDARY_GHOSTED, nx, dof, stencil_width, NULL, &domain);
-
-	 //Multiple pipes
-	DM* pipes;
+	for (int p = 0; p < PIPES_SIZE; p++) {
+		if (prc_owner[p] == rank) {
+			DM pipe;
+			DMDACreate1d(PETSC_COMM_SELF, DM_BOUNDARY_GHOSTED, nx, dof, stencil_width, NULL, &pipe);
+			DMCompositeAddDM(subdomain, pipe);
+		}
+	}
 
 	DM dm_redundant;
 	DMCreate(PETSC_COMM_WORLD, &dm_redundant);
 	DMSetType(dm_redundant, DMREDUNDANT);
-	RedundantSetSize(dm_redundant, 0, 1);
+	RedundantSetSize(dm_redundant, n_prcs - 1, 1);
 	DMSetUp(dm_redundant);
 
+	DMCompositeAddDM(subdomain, dm_redundant);
+	DMCompositeSetCoupling(subdomain, FormCoupleLocations);
+
 	DM domain;
-	DMCompositeCreate(PETSC_COMM_WORLD, &domain);
+	DMShellCreate(PETSC_COMM_WORLD, &domain);
 
-	PetscInt dof = 1;
-	PetscInt stencil_width = 1;
+	DMShellSetContext(domain, subdomain); 
+	DMShellSetCreateGlobalVector(domain, CreateGlobalVector);
+	DMShellSetCreateMatrix(domain, CreateMatrix);
 
-	for (int i = 0; i < PIPES_SIZE; i++) {
-		DM pipe;
-		DMDACreate1d(PETSC_COMM_WORLD, DM_BOUNDARY_GHOSTED, nx, dof, stencil_width, NULL, &pipe);
-		DMCompositeAddDM(domain, pipe);
-	}
+	//////////////////////////////////////////////////////////////
+	//////				Multiple pipes						////
+	//////////////////////////////////////////////////////////////
+	//DM* pipes;
 
-	DMCompositeAddDM(domain, dm_redundant);
+	//DM dm_redundant;
+	//DMCreate(PETSC_COMM_WORLD, &dm_redundant);
+	//DMSetType(dm_redundant, DMREDUNDANT);
+	//RedundantSetSize(dm_redundant, 0, 1);
+	//DMSetUp(dm_redundant);
 
-	DMCompositeSetCoupling(domain, FormCoupleLocations);
+	//DM domain;
+	//DMCompositeCreate(PETSC_COMM_WORLD, &domain);
+
+	//for (int i = 0; i < PIPES_SIZE; i++) {
+	//	DM pipe;
+	//	DMDACreate1d(PETSC_COMM_WORLD, DM_BOUNDARY_GHOSTED, nx, dof, stencil_width, NULL, &pipe);
+	//	DMCompositeAddDM(domain, pipe);
+	//}
+
+	//DMCompositeAddDM(domain, dm_redundant);
+
+	//DMCompositeSetCoupling(domain, FormCoupleLocations);
+
+	//////////////////////////////////////////////////////////////
 
 	TSSetDM(ts, domain);
 
 	Vec F;
 	DMCreateGlobalVector(domain, &F);
-	
+
 	//TSSetIFunction(ts, F, FormFunctionSinglePipe, &params);
 	TSSetIFunction(ts, F, FormFunction, &params);
 
@@ -125,8 +228,14 @@ int main(int argc, char **argv) {
 	
 	TSSetFromOptions(ts);
 	
-	//TSSolve(ts, x);
+	VecSet(F, 1.0);
+	VecAssemblyBegin(F);
+	VecAssemblyEnd(F);
 
+	VecView(F, PETSC_VIEWER_STDOUT_WORLD);
+	TSSolve(ts, x);
+
+	//VecView(x, PETSC_VIEWER_STDOUT_SELF);
 	//SNES snes;
 	//TSGetSNES(ts, &snes);
 	//SNESConvergedReason reason;
@@ -166,7 +275,8 @@ int main(int argc, char **argv) {
 
 	PetscFree(params.temperature_presc_);
 
-	DMDestroy(&domain);
+	DMDestroy(&subdomain);
+	//DMDestroy(&domain);
 	TSDestroy(&ts);
 
 	PetscFinalize();
